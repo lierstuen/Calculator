38fb65f4bf0ee18f47489d6c9f1d5a56be03aa28
Jonas Grimstad Lierstuen <jonagl18.uia.no>
2022-02-07 17:54:28 +0100

    Initial 2.0

---
+++ Calculator/Calculator/Controllers/AdapterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdapterController : ControllerBase
+    {
+        public AdapterController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetNum()
+        {
+            return Ok(45);
+        }
+    }
+}
